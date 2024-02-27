86c87c0ec7296959b5042e4f5e1fa25efbc37291
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2024-02-27 11:28:53 +0100

    Deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()]
+  integrations: [tailwind(), icon(), react()],
+  site: 'https://jonas-beck.github.io'
 });
